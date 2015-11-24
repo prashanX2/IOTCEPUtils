2db35bcb8966c7ac8cb1b584e928047ab92ef00c
prashan <prashan@compsoc.lk>
2015-11-24 12:42:30 +0530

    changed to tcp

--- src/main/java/com/iotceputils/Main.java
+++ src/main/java/com/iotceputils/Main.java
@@ -1,8 +1,9 @@
 package com.iotceputils;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class Main {
 
@@ -10,32 +11,47 @@ public class Main {
 
 
         try {
-            final DatagramSocket serverSocket = new DatagramSocket(44444);
+
+            String clientSentence;
+            String capitalizedSentence;
+            final ServerSocket serverSocket = new ServerSocket(44444);
+
+            //final DatagramSocket serverSocket = new DatagramSocket(44444);
 
             System.out.println(System.nanoTime());
 
-            System.out.println("Server startedd on main: " + serverSocket.getLocalAddress() + "@" + serverSocket.getLocalPort());
+            System.out.println("Server startedd on main: " + serverSocket.getInetAddress() + "@" + serverSocket.getLocalPort());
 
              byte[] receiveData = new byte[1024];
              byte[] sendData = new byte[1024];
 
             while(true)
             {
-                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-                serverSocket.receive(receivePacket);
-                String sentence = new String( receivePacket.getData());
+
+                Socket connectionSocket = serverSocket.accept();
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                clientSentence = inFromClient.readLine();
+                System.out.println("Received: " + clientSentence);
+                capitalizedSentence = clientSentence.toUpperCase() + '\n';
+                outToClient.writeBytes(capitalizedSentence);
+
+
+               // DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                //serverSocket.receive(receivePacket);
+                //String sentence = new String( receivePacket.getData());
                 //System.out.println("RECEIVED: " + sentence);
 
-                InetAddress IPAddress = receivePacket.getAddress();
-                int port = receivePacket.getPort();
+                //InetAddress IPAddress = receivePacket.getAddress();
+                //int port = receivePacket.getPort();
 
-                long s = System.nanoTime();
+                //long s = System.nanoTime();
 
-                String time =  String.valueOf(s);
-                sendData = time.getBytes();
-                DatagramPacket sendPacket =
-                        new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                serverSocket.send(sendPacket);
+               // String time =  String.valueOf(s);
+                //sendData = time.getBytes();
+               // DatagramPacket sendPacket =
+                       // new DatagramPacket(sendData, sendData.length, IPAddress, port);
+               // serverSocket.send(sendPacket);
                 System.out.println("sent");
             }
 
